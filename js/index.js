6610a64cfccfdafe02eeb0641dd50cede4f99aad
gaoshenk <15313160061@163.com>
2017-03-01 10:37:57 +0800

    git init

---
+++ js/index.js
@@ -0,0 +1,16 @@
+$(function(){
+	$('#B1').click(function(){
+		$('#mg3').removeClass().addClass('b1');
+	})
+	$('#B2').click(function(){
+		$('#mg3').removeClass().addClass('b2');
+	})
+	$('#B3').click(function(){
+		$('#mg3').removeClass().addClass('b3');
+	})
+	$('#B4').click(function(){
+		$('#mg3').removeClass().addClass('b4');
+	})
+	
+	})
+})
